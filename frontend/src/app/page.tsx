a64eca45afad2d8dc2a60ea5b5ae1a5b57f729d0
shotaro <850123.a@gmail.com>
2025-05-25 17:26:58 +0900

    api通信試験完了、daisyui適用、tailwindv3だと色変更適用ならずv4へ変更

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    console.log('useEffectが実行されました');
+    fetch("http://localhost:3000/hello")
+    .then((res) => {
+      console.log("レスポンスオブジェクト",res);
+       return res.json();
+    })
+    .then((data) => {
+      console.log("バース後のデータ", data);
+      return setMessage(data.message)
+    })
+    .catch((err) => {
+      console.log(err);
+      return setMessage("error");
+    });
+  },[]);
+
   return (
-    <div className="text-3xl font-bold underline">
-      Hello world!
+    <>
+    <div className="bg-base-100 min-h-screen">
+      <ul>
+        <li><a>{message}</a></li>
+        <li>{message}</li>
+        <li>{message}</li>
+        <li>{message}</li>
+      </ul>
+      <button className="btn btn-primary">ダミーボタン</button>
     </div>
+    </>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
@@ -6,13 +7,5 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  plugins: [daisyui],
 } satisfies Config;
