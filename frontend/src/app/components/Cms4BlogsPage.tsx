0fd55bd428ddf829647b2358248ab3927a957d2c
shotaro <850123.a@gmail.com>
2025-06-09 15:49:06 +0900

    Cms4BlogsPage.tsx作成

--- frontend/src/app/components/Cms4BlogsPage.tsx
+++ frontend/src/app/components/Cms4BlogsPage.tsx
@@ -7,16 +7,29 @@ import Image from "next/image";
 import Link from "next/link";
 import { CmsThumbnail } from "./CmsThumbnail";
 
-
 function Cms4BlogsPage({ cmsData }: { cmsData: BlogResponse }) {
   return (
     <>
+      <div className="flex items-center justify-between mt-4 ml-10">
+        <div className="cms-heading text-xl font-bold">ブログ記事</div>
+        <div>
+          <Link
+            href="blogs"
+            className="btn mr-10 justify-end c-btn-slide3 px-6 py-2 font-bold text-[#292929] bg-[#B1D9E9] border border-[#B1D9E9] rounded">
+            もっと見る
+          </Link>
+        </div>
+      </div>
+
       <div
         className="grid justify-items-center
     md:grid-cols-2 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-4">
         {cmsData.contents.map((blogs) => {
           return (
-            <Link href={`/blogs/${blogs.id}`} key={blogs.id} className="image-animation card bg-base-100 w-96 shadow-lg">
+            <Link
+              href={`/blogs/${blogs.id}`}
+              key={blogs.id}
+              className="image-animation card bg-base-100 w-96 shadow-lg">
               <figure>
                 <Image src={CmsThumbnail} alt="画像" width={400} height={300} priority={true} />
               </figure>
@@ -27,13 +40,6 @@ function Cms4BlogsPage({ cmsData }: { cmsData: BlogResponse }) {
             </Link>
           );
         })}
-      </div>
-      <div className="flex justify-end mt-4">
-        <Link
-          href="blogs"
-          className="btn mr-10 justify-end c-btn-slide3 px-6 py-2 font-bold text-[#292929] bg-[#B1D9E9] border border-[#B1D9E9] rounded">
-          もっと見る
-        </Link>
       </div>
     </>
   );
