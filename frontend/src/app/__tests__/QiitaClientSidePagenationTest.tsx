6049f80c002fc9c2c5f5e1673bcfdb71159e108b
shotaro <850123.a@gmail.com>
2025-06-16 15:07:15 +0900

    test/pagenation実装

--- frontend/src/app/__tests__/QiitaClientSidePagenationTest.tsx
+++ frontend/src/app/__tests__/QiitaClientSidePagenationTest.tsx
@@ -1,17 +1,17 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import CmsClientSidePagenation from "../components/pagenation/CmsClientSidePagenation";
 import userEvent from "@testing-library/user-event";
+import QiitaClientSidePagenation from "../components/pagenation/QiitaClientSidePagenation";
 
-describe("CmsClientSidePagenation", () => {
+describe("QiitaClientSidePagenation", () => {
   it("ページネーションが表示されていること", async () => {
-    render(<CmsClientSidePagenation currentPage={1} totalPages={10} onPageChange={function () {}} />);
+    render(<QiitaClientSidePagenation currentPage={1} totalPages={10} onPageChange={function () {}} />);
     expect(screen.getByRole("button", { name: "1" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "＜" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "＞" })).toBeInTheDocument();
   });
 
   it("現在のページがトータルページ-2よりも大きい場合にもページネーションが表示されていること", async () => {
-    render(<CmsClientSidePagenation currentPage={4} totalPages={5} onPageChange={function () {}} />);
+    render(<QiitaClientSidePagenation currentPage={4} totalPages={5} onPageChange={function () {}} />);
     expect(screen.getByRole("button", { name: "2" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "＜" })).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "＞" })).toBeInTheDocument();
@@ -23,7 +23,7 @@ describe("CmsClientSidePagenation", () => {
       currentPage = page;
     });
     const { rerender } = render(
-      <CmsClientSidePagenation currentPage={currentPage} totalPages={5} onPageChange={onPageChange} />
+      <QiitaClientSidePagenation currentPage={currentPage} totalPages={5} onPageChange={onPageChange} />
     );
 
     const button3 = screen.getByRole("button", { name: "3" });
@@ -32,9 +32,9 @@ describe("CmsClientSidePagenation", () => {
       expect(button3).toHaveBeenCalledWith(3);
     });
 
-    rerender(<CmsClientSidePagenation currentPage={currentPage} totalPages={5} onPageChange={onPageChange} />);
+    rerender(<QiitaClientSidePagenation currentPage={currentPage} totalPages={5} onPageChange={onPageChange} />);
     expect(button3).toHaveClass("text-white");
-    expect(button3).toHaveClass("bg-[#5696e3]");
+    expect(button3).toHaveClass("btn-primary");
   });
 
   it("＞を押すと", async () => {
@@ -43,13 +43,13 @@ describe("CmsClientSidePagenation", () => {
       return (currentPage = page);
     });
     const { rerender } = render(
-      <CmsClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />
+      <QiitaClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />
     );
 
     const nextButton = screen.getByRole("button", { name: "＞" });
     await userEvent.click(nextButton);
 
-    rerender(<CmsClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />);
+    rerender(<QiitaClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />);
     expect(screen.getByRole("button", { name: "7" })).toBeInTheDocument();
     screen.debug();
   });
@@ -60,13 +60,13 @@ describe("CmsClientSidePagenation", () => {
       return (currentPage = page);
     });
     const { rerender } = render(
-      <CmsClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />
+      <QiitaClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />
     );
 
     const prevButton = screen.getByRole("button", { name: "＜" });
     await userEvent.click(prevButton);
 
-    rerender(<CmsClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />);
+    rerender(<QiitaClientSidePagenation currentPage={currentPage} totalPages={15} onPageChange={onPageChange} />);
     expect(screen.getByRole("button", { name: "1" })).toBeInTheDocument();
   });
 });
